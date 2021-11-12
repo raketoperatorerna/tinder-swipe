d6c596eae5645c514846912500e1d7847b76dee3
gablas <gabla623@student.liu.se>
2021-11-12 20:28:19 +0100

    Init

---
+++ components/CardItem.tsx
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import {
+    Text,
+    View,
+    Image,
+    Dimensions,
+    TouchableOpacity,
+    Pressable,
+    Button,
+} from "react-native";
+import Icon from "./Icon";
+import { CardItemT } from "../types";
+import styles, {
+    DISLIKE_ACTIONS,
+    FLASH_ACTIONS,
+    LIKE_ACTIONS,
+    STAR_ACTIONS,
+    WHITE,
+} from "../assets/styles";
+import { TouchableHighlight } from "react-native-gesture-handler";
+
+const CardItem = ({
+    description,
+    hasActions,
+    hasVariant,
+    images,
+    isOnline,
+    matches,
+    name,
+}: CardItemT) => {
+    // Custom styling
+    const fullWidth = Dimensions.get("window").width;
+    const fullHeight = Dimensions.get("window").height;
+
+    const [showDescription, setShowDescription] = useState(false);
+
+    const [imageIndex, setImageIndex] = useState(0);
+
+    const imageStyle = [
+        {
+            //borderRadius: 8,
+            //width: hasVariant ? fullWidth / 2 - 30 : fullWidth - 80,
+            width: hasVariant ? fullWidth : fullWidth,
+            //height: hasVariant ? 170 : 350,
+            height: hasVariant ? fullHeight : fullHeight,
+            //margin: hasVariant ? 0 : 20,
+            margin: hasVariant ? 0 : 0,
+        },
+    ];
+
+    const nameStyle = [
+        {
+            //paddingTop: hasVariant ? 10 : 15,
+            //paddingBottom: hasVariant ? 5 : 7,
+            color: "#363636",
+            fontSize: hasVariant ? 15 : 30,
+            elevation: 5,
+            paddingBottom: 100,
+        },
+    ];
+
+    return (
+        <View style={styles.containerCardItem}>
+            {/* IMAGE */}
+            <Pressable
+                onPress={(e) => {
+                    setImageIndex(imageIndex == 0 ? 1 : 0);
+                    console.log(e)
+                }}
+                onLongPress={() => {
+                    setShowDescription(!showDescription);
+                }}
+            >
+                <Image source={images[imageIndex]} style={imageStyle} />
+            </Pressable>
+
+            {/* MATCHES */}
+            {matches && (
+                <View style={styles.matchesCardItem}>
+                    <Text style={styles.matchesTextCardItem}>
+                        <Icon name="heart" color={WHITE} size={13} /> {matches}%
+                        Match!
+                    </Text>
+                </View>
+            )}
+
+            {/* NAME */}
+            <Text style={nameStyle}>{name}</Text>
+
+            {/* DESCRIPTION */}
+            {showDescription && description && (
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        alignSelf: "stretch",
+                        bottom: 500,
+                        backgroundColor: "rgba(27, 32, 33, 1)",
+                        paddingBottom: 50,
+                    }}
+                >
+                    <Text style={styles.descriptionCardItem}>
+                        {description}
+                    </Text>
+                    <Button
+                        style={styles.button}
+                        title="Purchase"
+                        onPress={() => {}}
+                    ></Button>
+                </View>
+            )}
+
+            {/* STATUS */}
+            {!description && (
+                <View style={styles.status}>
+                    <View style={isOnline ? styles.online : styles.offline} />
+                    <Text style={styles.statusText}>
+                        {isOnline ? "Online" : "Offline"}
+                    </Text>
+                </View>
+            )}
+
+            {/* ACTIONS */}
+            {hasActions && (
+                <View style={styles.actionsCardItem}>
+                    <TouchableOpacity style={styles.miniButton}>
+                        <Icon name="star" color={STAR_ACTIONS} size={14} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button}>
+                        <Icon name="heart" color={LIKE_ACTIONS} size={25} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button}>
+                        <Icon name="close" color={DISLIKE_ACTIONS} size={25} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.miniButton}>
+                        <Icon name="flash" color={FLASH_ACTIONS} size={14} />
+                    </TouchableOpacity>
+                </View>
+            )}
+        </View>
+    );
+};
+
+export default CardItem;
